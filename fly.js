7886b7cc9e27d22688bd509f0753a4ec41ece631
ouyang <67744640+ldj-network@users.noreply.github.com>
2021-11-18 12:41:06 +0800

    Create fly.js
    add

---
+++ fly.js
@@ -0,0 +1,20 @@
+const SingleDay = 'luerhe.herokuapp.com'
+const DoubleDay = 'luerhe.herokuapp.com'
+addEventListener(
+    "fetch",event => {
+    
+        let nd = new Date();
+        if (nd.getDate()%2) {
+            host = SingleDay
+        } else {
+            host = DoubleDay
+        }
+        
+        let url=new URL(event.request.url);
+        url.hostname=host;
+        let request=new Request(url,event.request);
+        event. respondWith(
+            fetch(request)
+        )
+    }
+)
